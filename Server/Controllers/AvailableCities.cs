63a6a292949ab848b4936a48f970231c5f8d0558
Artur Yakupov <art.kazan@mail.ru>
2022-11-25 23:34:47 +0300

    city controller

---
+++ Server/Controllers/AvailableCities.cs
@@ -0,0 +1,16 @@
+﻿using Server.Models;
+using Server.Services;
+using Server.Services.ServerServices;
+
+namespace Server.Controllers;
+
+[ApiController("/")]
+public class AvailableCitiesController
+{
+    [HttpGet("city")]
+    public IActionResult AvailableCities()
+    {
+        var cities = new ORM().Select<AvailableCity>().Select(x => x.City).Distinct().ToArray();
+        return new TemplateView("Cities", new {cities = cities});
+    }
+}
